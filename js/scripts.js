dcb4d340a9d44863a6b97f5b04b2da39a2e4fc2b
Hamza Naeem and Nicko Dela Cruz <nicko@>
2018-01-09 14:20:24 -0800

    adds more definitions

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,16 @@ $(document).ready(function() {
     $("#v-show").slideToggle();
     $("#vnc-show").slideToggle();
     $("#f-show").slideToggle();
+    $("#m-show").slideToggle();
+    $("#a-show").slideToggle();
+    $("#p-show").slideToggle();
+    $("#r-show").slideToggle();
+    $("#c-show").slideToggle();
+    $("#s-show").slideToggle();
+
+
+
+
+
   });
 });
